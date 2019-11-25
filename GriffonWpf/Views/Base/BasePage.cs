f5a7e45a2d38a98d01f6beb07caeb10423a96047
harckline <antoinecronier1@gmail.com>
2019-11-25 15:08:20 +0100

    Use resource + const for Window default min

--- GriffonWpf/Views/Base/BasePage.cs
+++ GriffonWpf/Views/Base/BasePage.cs
@@ -10,6 +10,34 @@ namespace GriffonWpf.Views.Base
 {
     public abstract class BasePage : Page
     {
+        public BasePage()
+        {
+            this.Loaded += BasePage_Loaded;
+        }
+
+        private void BasePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            int minHeight = 0;
+            if (int.TryParse(GriffonWpf.Properties.Resources.AppMinHeight,out minHeight))
+            {
+                this.GetWindow().MinHeight = minHeight;
+            }
+            else
+            {
+                this.GetWindow().MinHeight = ViewsConstantes.DEFAULT_MIN_HEIGHT;
+            }
+
+            int minWidth = 0;
+            if (int.TryParse(GriffonWpf.Properties.Resources.AppMinWidth, out minWidth))
+            {
+                this.GetWindow().MinWidth = minWidth;
+            }
+            else
+            {
+                this.GetWindow().MinWidth = ViewsConstantes.DEFAULT_MIN_WIDTH;
+            }
+        }
+
         public Window GetWindow()
         {
             return Window.GetWindow(this);
---
+++ GriffonWpf/Views/Base/ViewsConstantes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GriffonWpf.Views.Base
+{
+    public static class ViewsConstantes
+    {
+        public const int DEFAULT_MIN_HEIGHT = 200;
+        public const int DEFAULT_MIN_WIDTH = 200;
+    }
+}
